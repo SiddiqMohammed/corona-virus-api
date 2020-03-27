db83e0d47c85aba4d721b9cb65e27df0459ee2cf
Siddiq Mohammed <msm2029@gmail.com>
2020-03-28 01:03:47 +0400

    First

---
+++ scraper.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+
+url = 'worldometer.com/corona'
+
+profile = webdriver.FirefoxProfile()
+profile.set_preference("media.volume_scale", "0.0")
+driver = webdriver.Firefox(firefox_profile=profile)
+driver.set_window_position(1500, -110)
+driver.get(url)
+
+time.sleep(10)
+
+html = driver.page_source
+soup = BeautifulSoup(html, "lxml")
+
+for tag in soup.find('div', {"class" : "maincounter-number"}):
+    print(tag.text)
+
+
+driver.close()
