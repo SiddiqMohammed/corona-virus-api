bfee81e01ea56b846d3804f007288958af1cdad7
Siddiq Mohammed <msm2029@gmail.com>
2020-03-28 05:43:18 +0400

    Python to firebase done

--- scraper.py
+++ scraper.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
+from firebase import firebase
+
 
 url = 'https://www.worldometers.info/coronavirus/'
 
@@ -9,7 +11,9 @@ profile.set_preference("media.volume_scale", "0.0")
 driver = webdriver.Firefox(firefox_profile=profile)
 #driver.set_window_position(1500, -110)
 driver.get(url)
+firebase = firebase.FirebaseApplication("https://pythondb-bfd79.firebaseio.com/", None)
 
+x = ''
 
 def updates():
     time.sleep(1)
@@ -18,16 +22,33 @@ def updates():
     soup = BeautifulSoup(html, "lxml")
 
     for tag in soup.find('div', {"class" : "maincounter-number"}):
-        x = ""
+        # print(tag)
         for child in tag.string:
-            x = x + child
-        print(x)
+            global x
+            x = x + child.rstrip()
+        # print(child)
+        # print(x)
+        # data = {
+        # 'Name': 'adasd',
+        # 'Email': 'adasdasd@jason.com',
+        # 'Phone': 1234567890
+        # }
+        if x != "":
+            
+            result = firebase.put('/pythondb-bfd79/customer/-M3TXyKe-R9-Vkk4MsEI', 'Name', x)
+            # print(result)
 
-for i in range(48):       #for 24hrs range = 48
-    Start()
-    time.sleep(1800)    #60s*30 = 30mins = 1800
+# for i in range(48):       #for 24hrs range = 48
+#     Start()
+#     time.sleep(1800)    #60s*30 = 30mins = 1800
 
 def Start():
     updates()
 
+
+
+Start()
+
 driver.close()
+
+
