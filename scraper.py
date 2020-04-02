2997de89947c14d8222c9b5526137f111294cf6c
Siddiq Mohammed <msm2029@gmail.com>
2020-04-02 05:05:11 +0400

    Increased Scrape Content

--- scraper.py
+++ scraper.py
@@ -23,34 +23,58 @@ firebase = firebase.FirebaseApplication(Database_link, None)
 
 #Updating Firebase Database
 def updates():
-    time.sleep(1)
+    time.sleep(5)
 
     html = driver.page_source
     soup = BeautifulSoup(html, "lxml")
 
     listed = []
-    text_list = ['Total_Cases', 'Deaths', 'Recovered']
+    listed1 = []
+    text_list = ['Mild', 'Serious', 'Recovered', 'Deaths', 'Active', 'Closed', 'Total']
     i = 0
    
-    for my_tag in soup.find_all(class_="maincounter-number"):
+   
+    for my_tag in soup.find_all(class_="number-table"):
         listed.append(my_tag.text.strip())
-    while i < len(text_list):
-        result = firebase.put(put_link, "'{}'".format(text_list[i]), listed[i])
+
+    for i in range(len(listed)):
+        x = listed[i]
+        x = x.replace(",", "")
+        listed1.append(int(x))
+
+    
+    listed1.append(listed1[0] + listed1[1])
+    listed1.append(listed1[2] + listed1[3])
+    listed1.append(listed1[4] + listed1[5])
+
+    print(listed1)
+    i = 0
+
+    while i < len(listed1):
+        result = firebase.put(put_link, "'{}'".format(text_list[i]), listed1[i])
         i += 1
+    
+
+
 
 #Main Function Starts Seperate Which Will Be Useful If Adding Functions In The Future
 def Start():
     updates()
 
+
 #Running The Code Forever
 # while True:
-#     Start()
+#     Start()     
+#     print("updated")
+#     time.sleep(1800)  #1800s = 30mins
+#     driver.refresh()
 
 #Running The Code For A Specific Number Of times Or Specific Amount Of Time
-for i in range(2):       #Number Of Times
-    print("update")
-    time.sleep(10)    #Time In Seconds 
+for i in range(1):       #Number Of Times
+    time.sleep(1)    #Time In Seconds 
     Start()
+    print("update")
+
 
 driver.close()
 
